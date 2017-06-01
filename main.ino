cfd7041a39a5c540e3e42b76698134b7db1b5af1
Jonatan Hägglund <jonatan@linux.com>
2017-06-01 20:19:37 +0200

    Update main.ino
    lagt till lite till

--- main.ino
+++ main.ino
@@ -12,16 +12,39 @@ DallasTemperature DS18B20(&oneWire);
 char temperatureString[6];
 
 
- float ReadDS18B20Temperature() {
+ void ReadDS18B20Temperature() {
  // Serial.print("Requesting DS18B20 temperature...");
   float temp;
-  do {
-    DS18B20.requestTemperatures(); 
+  float temp1;
+  
+   /* 
+    
     temp = DS18B20.getTempCByIndex(0);
-   // Serial.println("sensorn fick: " + String(temp)); 
+    Serial.print("sensor 1: ");
+   Serial.println(temp);
+
+    temp1 = DS18B20.getTempCByIndex(1);
+        Serial.print("sensor 2: ");
+   Serial.println(temp1);
+
+   */
+
+  for (int i = 0; i < 2; i++){
+    do {
+      DS18B20.requestTemperatures(); 
+      temp = DS18B20.getTempCByIndex(i);
+      // TODO lägg till säkerhetskoll så att vi inte fastnar här i all evighet
+    } while (temp == 85.0 || temp == (-127.0));
+    
+    Serial.print("sensor ");
+    Serial.print( (i + 1) );
+    Serial.print(": ");
+    Serial.println(temp);  
+  }
+   
     delay(100);
-  } while (temp == 85.0 || temp == (-127.0));
-  return temp;
+  
+//  return temp;
   }
   
 void setup() {
@@ -35,9 +58,9 @@ void loop() {
   // put your main code here, to run repeatedly:
 
  
- float myTemp = ReadDS18B20Temperature(); // V1.3 -- läser temperatur från DS18B20
+ ReadDS18B20Temperature(); // V1.3 -- läser temperatur från DS18B20
 
- Serial.println(myTemp);
- delay(6);
+ //Serial.println(myTemp);
+ delay(1000);
 
 }
